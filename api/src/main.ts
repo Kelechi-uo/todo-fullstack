73a9e97ed218621d9bb96465169b25ef5c8197f1
Kelechi Okere <kelechinet@gmail.com>
2025-09-03 19:34:05 +0100

    Day 2: dev live-reload & clean script

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,5 +4,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3001);
+console.log("✅ Hot reload is working very well!");
+
 }
 bootstrap();
+// Test change for hot reload
+// Test change for hot reload
